6f2bc311ed94b157adbe9e73dfe5958749acecd0
ljmissir <ljmissir@163.com>
2020-07-05 11:25:48 +0800

    实现vuex下的gutters

--- src/kstore/kvuex.js
+++ src/kstore/kvuex.js
@@ -4,23 +4,29 @@ class Store {
     constructor (options) {
         this._mutations = options.mutations
         this._actions = options.actions
+        this._wrapperGutters = options.getters
 
-        // this指向绑定
-        const store = this
-        const {commit, action} = this
-        this.commit = function boundCommit (type, payload) {
-            commit.call(store, type, payload)
-        }
-        this.action = function boundAction (type, payload) {
-            return action.call(store, type, payload)
-        }
         // 初始化当前getters
-        let curGetters = {}
-        // 从Store中取gettes进行遍历
-        Object.keys(options.getters).forEach(key => { 
-            curGetters[key] = () => {
-                return options.getters[key](options.state)
-            }       
+        // let curGetters = {}
+        // // 从Store中取gettes进行遍历
+        // Object.keys(options.getters).forEach(key => { 
+        //     curGetters[key] = () => {
+        //         return options.getters[key](options.state)
+        //     }       
+        // })
+
+        let computed = {}
+        this.getters = {}
+        const store = this
+
+        Object.keys(this._wrapperGutters).forEach(key => {
+            const fn = store._wrapperGutters[key]
+            computed[key] = function () {
+                return fn(store.state)
+            }
+            Object.defineProperty(store.getters, key, {
+                get: () => store._vm[key]
+            })
         })
 
         // 设置我响应式数据
@@ -28,19 +34,23 @@ class Store {
             data: {
                 $$state: options.state
             },
-            computed: curGetters
+            computed
         })
-        console.log('天王盖地虎', '学习使我快乐')
+        console.log('天王盖地虎', '学习使我快乐', '今天不学习，明天变垃圾')
+
+        // this指向绑定
+        const {commit, action} = this
+        this.commit = function boundCommit (type, payload) {
+            commit.call(store, type, payload)
+        }
+        this.action = function boundAction (type, payload) {
+            return action.call(store, type, payload)
+        }
     }
 
     // 取state
     get state () {
         return this._vm._data.$$state
-    }
-
-    // 取gettes
-    get getters() {
-        return this._vm
     }
 
     commit(type, payload) {
